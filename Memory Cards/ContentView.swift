30870d7b3e308a9b1299ecdfa1b00b21aec16bfc
Konstantin Loginov <loginov.k@gmail.com>
2022-09-09 09:15:59 +0200

    Initial Commit

---
+++ Memory Cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory Cards
+//
+//  Created by Konstantin Loginov on 09/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memory Cards/Memory_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_CardsApp.swift
+//  Memory Cards
+//
+//  Created by Konstantin Loginov on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Memory_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
