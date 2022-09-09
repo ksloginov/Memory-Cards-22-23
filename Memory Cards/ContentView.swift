15fa9138b3b2cf4c2bf59975349a819f2677d613
Konstantin Loginov <loginov.k@gmail.com>
2022-09-09 10:48:32 +0200

    Added Add/Remove buttons

--- Memory Cards/ContentView.swift
+++ Memory Cards/ContentView.swift
@@ -8,15 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    // var emojies = ["❤️", "👨🏻‍🍳", "💩", "😡"]
-    // var emojies = ["❤️", "👨🏻‍🍳", "💩", "😡", "😧", "🤖", "🎩", "🐯", "🦺", "💍", "🎒", "🐎", "🐩", "🦧", "🐳", "🐕", "🦄", "🦀", "🐿", "🦔", "🦦", "🦢", "🦜"]
+    //let emojies = ["❤️", "👨🏻‍🍳", "💩", "😡"]
+    var emojies = ["❤️", "👨🏻‍🍳", "💩", "😡", "😧", "🤖", "🎩", "🐯", "🦺", "💍", "🎒", "🐎", "🐩", "🦧", "🐳", "🐕", "🦄", "🦀", "🐿", "🦔", "🦦", "🦢", "🦜"]
+    
+    @State var emojiCount: Int = 5
     
     var body: some View {
-        HStack {
-            CardView(content: "🥲")
-            CardView(content: "🎯")
-            CardView(content: "⛳️")
-            CardView(content: "🐼")
+        VStack {
+            HStack {
+                ForEach(emojies[0..<emojiCount], id: \.self ) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack {
+                removeButton
+                Spacer()
+                addButton
+            }
         }
+    }
+    
+    private var removeButton: some View {
+        Button {
+            if emojiCount > 0 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle.fill")
+                .resizable()
+                .frame(width: 56.0, height: 56.0)
+        }
+        .padding()
+    }
+    
+    private var addButton: some View {
+        Button {
+            if emojiCount < emojies.count - 1 {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle.fill")
+                .resizable()
+                .frame(width: 56.0, height: 56.0)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews1: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
